05812175d68dc45dc4f8aa9b318ae0b496dbf0b4
xchaix <xchai@189.cn>
2023-03-17 10:28:05 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -7,4 +7,4 @@ sed -i 's/192.168.1.1/192.168.9.199/g' openwrt/package/base-files/files/bin/conf
 #sed -i 's?zstd$?zstd ucl upx\n$(curdir)/upx/compile := $(curdir)/ucl/compile?g' tools/Makefile
 #sed -i 's/$(TARGET_DIR)) install/$(TARGET_DIR)) install --force-overwrite/' package/Makefile
 #sed -i 's/root:.*/root:$1$tTPCBw1t$ldzfp37h5lSpO9VXk4uUE\/:18336:0:99999:7:::/g' package/base-files/files/etc/shadow
-sed -i 's/# CONFIG_PACKAGE_kmod-ixgbevf is not set/CONFIG_PACKAGE_kmod-ixgbevf=y/g' feeds/smpackage/.github/diy/${{matrix.target}}.config
+sed -i 's/# CONFIG_PACKAGE_kmod-ixgbevf is not set/CONFIG_PACKAGE_kmod-ixgbevf=y/g' feeds/smpackage/.github/diy/Lean.config
